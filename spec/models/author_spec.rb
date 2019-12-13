a18b46673834750d0dc6de386e806de92bd2bb3f
bwatson78 <vagrant@vagrant.vm>
2019-12-13 23:11:42 +0000

    tests whether all new attributes for both models are working.

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to respond_to(
+    :email,
+    :password,
+    :first_name,
+    :middle_name,
+    :last_name,
+    :home_town,
+    :home_state,
+    :dob)
+  }
+  it { is_expected.to respond_to(
+    :reset_password_token,
+    :reset_password_sent_at,
+    :sign_in_count,
+    :current_sign_in_at,
+    :last_sign_in_at,
+    :current_sign_in_ip,
+    :last_sign_in_ip,
+    :books)
+  }
 end
