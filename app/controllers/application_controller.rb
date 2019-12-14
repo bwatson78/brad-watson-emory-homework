10787a69df235bdf4af1dbd462433cfe7783e21c
bwatson78 <vagrant@vagrant.vm>
2019-12-14 16:43:11 +0000

    ensures that devise works with my custom attributes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def home
     render :home
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    attributes = [
+      :first_name,
+      :middle_name,
+      :last_name,
+      :home_town,
+      :home_state,
+      :dob
+    ]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
   end
 end
